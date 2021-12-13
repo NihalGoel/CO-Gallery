6ef5ab4e130b1259ffd78939c30c68d703efd7cf
ngoel <ngoel@bol.com>
2021-12-13 09:43:21 +0100

    Add some mobile css and folder for fontfaces

--- components/post-body.js
+++ components/post-body.js
@@ -4,8 +4,8 @@ import container from './container.module.css'
 export default function PostBody({ content }) {
 
   const regexToSelectWordsInsideParagraphs = /(\w+(?![^<>]*>))/igm
+  console.log(content)
   content = content.toString().replace(regexToSelectWordsInsideParagraphs, '<span>$1</span>')
-
   return (
       <div
         className={markdownStyles['markdown'], container.body}
