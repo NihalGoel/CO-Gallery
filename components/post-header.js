63bec6bc3a76531340574ad634de00ac3fab46db
ngoel <ngoel@bol.com>
2021-12-14 00:10:11 +0100

    add adrian sauer artist info dummy url

--- components/post-header.js
+++ components/post-header.js
@@ -4,6 +4,7 @@ export default function PostHeader({ title }) {
   return (
     <>
       <a href="/" className="clouds-glossary">Clouds Glossary</a>
+      <a href="/" className="adrian-sauer">&#8730;</a>
       <PostTitle>{title}</PostTitle>
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ export default function Index({posts, children}) {
       return (
           <div className={container.container}>
             <a href="/" className="clouds-glossary">Clouds Glossary</a>
+            <a href="/" className="adrian-sauer">&#8730;</a>
             <div className={container.contentContainer}></div>
             <Menu>
               {posts.length > 0 && <Posts posts={posts}></Posts>}
