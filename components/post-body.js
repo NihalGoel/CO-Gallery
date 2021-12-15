72de654f0a7e2e039799367c06a948307e67a259
ngoel <ngoel@bol.com>
2021-12-15 22:05:05 +0100

    remove console log lines

--- components/post-body.js
+++ components/post-body.js
@@ -3,13 +3,11 @@ import container from './container.module.css'
 
 export default function PostBody({ content }) {
   const selectEveryWordInsideP = /(\w+(?![^<>]*>))/igm
-  console.log(content)
   content = content.toString()
     .replace(/(<\/p>\n<h2>)/igm, ' <span class="tooltip"><sup><strong>1</strong></sup><span class="tooltip-text">')
     .replace(/(<\/h2>)/igm, '</span></span></p>')
-  // console.log(content)
-  content = content.toString().replace(selectEveryWordInsideP, '<span class="post-text">$1</span>')
-  console.log(content)
+    .replace(selectEveryWordInsideP, '<span class="post-text">$1</span>')
+
   return (
     <div className="post-body">
       <div
