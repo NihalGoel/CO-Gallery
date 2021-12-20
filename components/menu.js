28ba3a4e73a709f1e3ef00349da89ce84faec9eb
ngoel <ngoel@bol.com>
2021-12-21 00:42:34 +0100

    Implement infinite scrolling

--- components/menu.js
+++ components/menu.js
@@ -2,8 +2,13 @@ import menu from '../components/menu.module.css';
 
 export default function Menu({ children }) {
   return <div className={menu.container}>
+    <div className={'menu-item'}>
     <div
-      className={`menu-item ${menu.text}`}
+      className={`menu-item1 ${menu.text}`}
     >{children}</div>
+    <div
+      className={`menu-item2 ${menu.text}`}
+    >{children}</div>
+    </div>
   </div>
 }
