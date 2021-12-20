c5206bd33dcb37a0f98bd68890e5d411a8f79af3
NullSense <matas234@gmail.com>
2021-12-20 19:58:58 +0100

    Test jquery

--- components/menu.js
+++ components/menu.js
@@ -1,9 +1,25 @@
 import menu from '../components/menu.module.css';
+import React, { useEffect, useRef } from 'react';
+import $ from 'jquery';
+import  'jquery.marquee';
 
 export default function Menu({ children }) {
-  return <div className={menu.container}>
-    <div
-      className={`menu-item ${menu.text}`}
-    >{children}</div>
-  </div>
+  const el = useRef();
+
+  useEffect(function() {
+    const $el = $(el.current);
+
+    $el.marquee({
+      duration: 5000,
+      gap: 50,
+      delayBeforeStart: 0,
+      direction: 'left'
+    });
+  });
+
+  return (
+    <div ref={el}>
+      I'm using jQuery.Marquee with React!!!!
+    </div>
+  );
 }
