0b574b1985a0f89b79997109077bbac943403c6e
ngoel <ngoel@bol.com>
2021-12-21 21:15:25 +0100

    fix footer number formatting

--- components/post-body.js
+++ components/post-body.js
@@ -3,12 +3,14 @@ import container from './container.module.css'
 
 export default function PostBody({ content }) {
   const selectEveryWordInsideP = /(\w+(?![^<>]*>))/igm
+  console.log(content)
   content = content.toString()
     .replace(/(-!)/gm, '<span class="tooltip"><sup><strong>')
     .replace(/(!-)/gm, '</strong></sup><span class="tooltip-text">')
     .replace(/(<\/p>\n<h2>)/igm, '')
     .replace(/(<\/h2>)/igm, '</span></span></p>')
     .replace(selectEveryWordInsideP, '<span class="post-text">$1</span>')
+  console.log(content)
 
   return (
     <div className="post-body">
