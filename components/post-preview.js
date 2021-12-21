40809bb24c85785b483fbc90c4baebece743812e
ngoel <ngoel@bol.com>
2021-12-21 02:02:00 +0100

    add vertical linespacing on infinite scroll

--- components/post-preview.js
+++ components/post-preview.js
@@ -1,14 +1,17 @@
 import Link from 'next/link'
+import { useRouter } from "next/router";
 
 export default function PostPreview({
   title,
   slug,
 }) {
+  const router = useRouter();
+  const isActive = router.asPath.endsWith(slug.toString())
   return (
     <div>
       <h3>
         <Link as={`/posts/${slug}`} href="/posts/[slug]">
-          <a>{title}</a>
+          <a className={isActive ? "active" : "inactive"}>{title}</a>
         </Link>
       </h3>
     </div>
